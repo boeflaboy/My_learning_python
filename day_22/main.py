e6e8dcc187bdae0182e7e1953a7e2d0c0f8229af
boeflaboy <s-ridha@live.nl>
2023-06-27 11:13:50 +0200

    faewhf

--- day_22/main.py
+++ day_22/main.py
@@ -2,10 +2,25 @@ from turtle import Screen
 from ping_pong_components import RightPaddle
 
 screen = Screen()
+
+screen.tracer(0)
+
 screen.setup(width=800, height=600)
 screen.bgcolor('black')
 screen.title('Ping Pong Game in Python')
 
-right_paddle = RightPaddle()
+paddle = RightPaddle()
+
+
+screen.listen()
+
+screen.onkeypress(paddle.move_up, "Up")
+screen.onkeypress(paddle.move_down, "Down")
+
+while True:
+    screen.update()
+
+
+
 
 screen.exitonclick()
--- day_22/ping_pong_components.py
+++ day_22/ping_pong_components.py
@@ -1,15 +1,28 @@
 from turtle import Turtle
 
-RIGHT_PADDLE_POSTION_X = 350
-RIGHT_PADDLE_POSTION_Y = 0
 
 
 
-
-class RightPaddle(Turtle):
+class RightPaddle:
     def __init__(self):
-        super().__init__()
-        self.shape('square')
-        self.shapesize(stretch_wid=20)
-        self.goto(RIGHT_PADDLE_POSTION_X, RIGHT_PADDLE_POSTION_Y)
-        self.color('white')
+        self.part = Turtle(shape='square')
+        self.part.shapesize(stretch_wid=5, stretch_len=1)
+        self.part.penup()
+        self.part.color('white')
+        self.part.goto(350,0)
+
+
+    def move_up(self):
+        new_y = self.part.ycor() + 20
+        self.part.goto(self.part.xcor(), new_y)
+
+    def move_down(self):
+        new_y = self.part.ycor() - 20
+        self.part.goto(self.part.xcor(), new_y)
+
+
+
+
+
+
+
